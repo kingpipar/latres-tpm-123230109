ca7030fd744e50536040e3d40ff7819026453213
kingpipar <masfaremmm@gmail.com>
2026-05-21 12:51:52 +0700

    fix: notif dependencies

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,9 +11,14 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
+
+    dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
+    }   
 
     kotlinOptions {
         jvmTarget = JavaVersion.VERSION_17.toString()
